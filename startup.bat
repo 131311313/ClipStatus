4e80c9b339d3f07d79aa21e5429ff0fc989838af
oscar <77478328+131311313@users.noreply.github.com>
2025-04-13 15:56:48 +0900

    Delete startup.bat

--- startup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-start "" pythonw.exe "C:\Program Files (x86)\clipstatus\ClipStatus.pyw"
-if errorlevel 1 (
-    echo Error occurred while starting the script.
-    pause
-) else (
-    exit
-)
